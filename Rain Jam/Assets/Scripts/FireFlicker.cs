8e23eb9826ef2b3a6d26a6f26c64dd10d8dbdae7
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-11-16 14:47:47 -0500

    More level + flickering fire

---
+++ Rain Jam/Assets/Scripts/FireFlicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlicker : MonoBehaviour
+{
+    [SerializeField] private float MinIntensity;
+    [SerializeField] private float MaxIntensity;
+    [SerializeField] private float TimeDelta = 0.0f;
+    [SerializeField] private Light light;
+
+    private float timer = 0.0f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+        if(timer >= TimeDelta)
+        {
+            timer = 0.0f;
+            light.intensity = Random.Range(MinIntensity, MaxIntensity);
+        }
+    }
+
+    private void OnValidate()
+    {
+        if(light == null)
+        {
+            light = GetComponent<Light>();
+        }
+    }
+}
