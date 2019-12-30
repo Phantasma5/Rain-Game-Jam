5a2e7ffaedc1f1b590d131f2086814ddf84703b8
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-12-30 15:52:33 -0500

    Stuff for footage

---
+++ Rain Jam/Assets/Marketing-Materials/Scripts/FollowTransform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform FollowTarget;
+    public Vector3 FollowOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(FollowTarget)
+        {
+            FollowOffset = transform.position - FollowTarget.position;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = FollowTarget.position + FollowOffset;
+    }
+}
